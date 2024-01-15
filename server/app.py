2b0843a5706bb8793ee28913206b10a22aaa9d60
bill <bill.arnold@student.moringaschool.com>
2024-01-15 21:00:12 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    messages_json = [message.to_dict() for message in messages]
+    return jsonify(messages_json)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict())
 
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if message:
+        data = request.get_json()
+        message.body = data.get('body', message.body)
+        db.session.commit()
+        return jsonify(message.to_dict())
+    return jsonify({'error': 'Message not found'}), 404
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({'message': 'Message successfully deleted'})
+    return jsonify({'error': 'Message not found'}), 404
+
+# Run the application
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
